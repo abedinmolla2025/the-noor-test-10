6b273e859dd3812cb2a99cbb7fe3c7c9f1205f96
abedinmolla2025 <abedinmolla2025@gmail.com>
2025-12-29 16:40:20 +0530

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-{
-  "appId": "com.noor.app",
-  "appName": "NOOR",
-  "webDir": "www",
-  "server": {
-    "url": "https://remix-of-remix-of-remix-of-islamic.vercel.app",
-    "cleartext": true
-  }
-}
